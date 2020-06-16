92ff71519445e4226582c977f72c37cb9f331d5e
tpmccallum <mistermac2008@gmail.com>
2020-06-16 14:37:35 +1000

    Regex vs orig

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 exports.convert = function(_string){
-    const re = new RegExp('\,');
-    var array = _string.match(re, '$&').input;
+    temp = '';
+    array = [];
+    for (c of _string) {
+        if (c == ",") {
+            array.push(Number(temp));
+            temp = '';
+        } else {
+            if (temp.length == 0) {
+                temp = c;
+            } else {
+                temp = temp + c;
+            }
+        }
+    }
+    if (temp.length > 0){
+        array.push(Number(temp));
+    }
     return array;
 }
